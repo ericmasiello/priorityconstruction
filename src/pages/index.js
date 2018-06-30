01f0d2f49024a74a1975265c8db4d6b92576e875
Eric Masiello <eric.j.masiello@gmail.com>
2018-06-30 19:41:17 -0400

    Move components to styleguide page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import PostListing from '../components/Posts/PostListing';
-import Type1 from '../components/Type1';
-import Type2 from '../components/Type2';
-import Type3 from '../components/Type3';
-import Type4 from '../components/Type4';
-import Type5 from '../components/Type5';
-import Small from '../components/Small';
-import Base from '../components/Base';
-
 
 const IndexPage = ({ data }) => (
   <div>
-    <Type1>Posts</Type1>
-    <Type2>Posts</Type2>
-    <Type3>Posts</Type3>
-    <Type4>Posts</Type4>
-    <Type5>Posts</Type5>
-    <Base>Posts</Base>
-    <Small>Posts</Small>
     {data.allMarkdownRemark.edges.map(({ node }) => <PostListing key={node.id} post={node} />)}
   </div>
 );
