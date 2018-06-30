d22ba621e5f074a07b404f8a40120024d6fe9cdc
Eric Masiello <eric.j.masiello@gmail.com>
2018-06-30 14:26:48 -0400

    Add type vars and mixins

--- src/styles/mixins.js
+++ src/styles/mixins.js
@@ -1,2 +1,30 @@
+import { pxToRem } from './utils';
+
 export const baseFont = () => 'font-family: \'Source Sans Pro\', sans-serif;';
 export const headerFont = () => 'font-family: \'Lato\', sans-serif;';
+
+export const type = typeDef => (
+  `
+    font-size: ${pxToRem(typeDef[0])};
+    line-height: ${typeDef[1]};
+  `
+);
+
+export const scalableType = (
+  [maxSize, lineHeight],
+  minSize = 16,
+) => {
+  const relativeScaler = 10;
+  return `
+    font-size: 10vw;
+    line-height: ${lineHeight};
+
+    @media(max-width: ${pxToRem(minSize * relativeScaler)}) {
+      font-size: ${pxToRem(minSize)};
+    }
+
+    @media(min-width: ${pxToRem(maxSize * relativeScaler)}) {
+      font-size: ${pxToRem(maxSize)};
+    }
+  `;
+};
