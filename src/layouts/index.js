915797ce4ea2919d28fba8fec85882f9fa7b25cd
Eric Masiello <eric.j.masiello@gmail.com>
2018-07-01 11:32:35 -0400

    Refactor Header component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { injectGlobal } from 'styled-components';
-
+import Link from 'gatsby-link';
+import styled, { injectGlobal } from 'styled-components';
 import Header from '../components/Header';
 import PageContainer from '../components/PageContainer';
+import FlatList from '../components/FlatList';
 import base from '../styles/base.css';
 
 // eslint-disable-next-line no-unused-expressions
 injectGlobal`
   ${base}
+`;
+
+const Logo = styled.h1`
+  ${props => `background-image: url('${props.image}');`}
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 100%;
+  background-size: contain;
+  text-indent: -9999px;
+  overflow: hidden;
+  margin: 0;
+`;
+
+const LogoLink = styled(Link)`
+  display: inline-block;
+  height: 100px;
+  width: 440px;
+`;
+
+const MainNav = styled.nav`
+  a {
+    color: #fff;
+    &:hover {
+      border-bottom: 3px solid #524763;
+    }
+  }
 `;
 
 const TemplateWrapper = ({ children, data, location }) => (
@@ -22,10 +49,25 @@ const TemplateWrapper = ({ children, data, location }) => (
       ]}
     />
     <Header
-      title={data.site.siteMetadata.title}
-      data={data}
-      location={location}
-    />
+      bgImageSizes={data.background.sizes}
+      isFullHeight={location.pathname === '/'}
+    >
+      <LogoLink to="/">
+        <Logo image={data.logo.sizes.src}>
+          {data.site.siteMetadata.title}
+        </Logo>
+      </LogoLink>
+      <MainNav>
+        <FlatList>
+          <FlatList.Item>
+            <Link to="/">Home</Link>
+          </FlatList.Item>
+          <FlatList.Item>
+            <Link to="/about">About</Link>
+          </FlatList.Item>
+        </FlatList>
+      </MainNav>
+    </Header>
     <PageContainer>
       {children()}
     </PageContainer>
