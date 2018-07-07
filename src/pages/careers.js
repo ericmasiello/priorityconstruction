e2dca5bbef351f9bcbec7476c702a1d46cc6e06a
Eric Masiello <eric.j.masiello@gmail.com>
2018-07-07 15:29:35 -0400

    Add stub for career page

---
+++ src/pages/careers.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Type1 from '../components/Type1';
+
+const Careers = () => (
+  <div>
+    <Type1>Placeholder</Type1>
+    <p>
+      lorem ipsum.
+    </p>
+  </div>
+);
+
+Careers.displayName = 'Careers';
+
+export default Careers;
