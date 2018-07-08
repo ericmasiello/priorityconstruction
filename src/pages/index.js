eb2b64f962134b1b4a17aeac0b500c687cae263b
Eric Masiello <eric.j.masiello@gmail.com>
2018-07-08 19:54:24 -0400

    Add beginning PhotoGrid to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { AllImageSharpPropTypes } from '../propTypes';
-import GatsbyImage from '../components/GatsbyImage';
-import List from '../components/List';
+import PhotoGrid from '../components/PhotoGrid';
 
-const HomePage = ({ data }) => (
-  <div>
-    <p>
-      Priority Construction Corp., is dedicated to complete different types
-      of concrete projects throughout the Baltimore area by using highly
-      skilled employees and paying attention to every detail of the project.
-    </p>
-    <List>
-      {data.homageGallery.edges.map(edge => (
-        <GatsbyImage
-          key={edge.node.id}
-          sizes={edge.node.sizes}
-        />
-      ))}
-    </List>
-  </div>
-);
+// TODO: move these to a utility file
+const predicate = (primaryMatch) => (include) => (edge) => {
+  if (edge.node.id.includes(primaryMatch)) {
+    return include;
+  }
+  return !include;
+};
+
+const mapEdgeToNode = edge => edge.node;
+
+const edgesToGallery = (edges, primaryMatch = '') => {
+  const predicateMatch = predicate(primaryMatch);
+  const primary = edges.find(predicateMatch(true));
+
+  if (primary) {
+    return {
+      primaryImage: mapEdgeToNode(primary),
+      additionalImages: edges.filter(predicateMatch(false)).map(mapEdgeToNode),
+    };
+  }
+
+  return {
+    primaryImage: null,
+    additionalImages: edges.map(mapEdgeToNode),
+  };
+};
+
+const HomePage = (props) => {
+  const { data } = props;
+  const photoGridProps = edgesToGallery(data.homageGallery.edges, 'courtyard.jpg');
+
+  return (
+    <div>
+      <p>
+        Priority Construction Corp., is dedicated to complete different types
+        of concrete projects throughout the Baltimore area by using highly
+        skilled employees and paying attention to every detail of the project.
+      </p>
+      <PhotoGrid {...photoGridProps} />
+    </div>
+  );
+};
 
 HomePage.displayName = 'HomePage';
 
