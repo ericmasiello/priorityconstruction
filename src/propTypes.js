640c4eda3bd336eecef4f4040de23c0841292f65
Eric Masiello <eric.j.masiello@gmail.com>
2018-07-07 20:37:30 -0400

    Create shared library of propTypes

---
+++ src/propTypes.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+export const ImageSharpPropTypes = PropTypes.shape({
+  id: PropTypes.string,
+  sizes: PropTypes.shape({
+    aspectRatio: PropTypes.number,
+    base64: PropTypes.string,
+    sizes: PropTypes.string,
+    src: PropTypes.string,
+    srcSet: PropTypes.string,
+  }),
+});
+
+export const AllImageSharpPropTypes = PropTypes.shape({
+  edges: PropTypes.arrayOf(PropTypes.shape({
+    node: ImageSharpPropTypes,
+  })),
+});
