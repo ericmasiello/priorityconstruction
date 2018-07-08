3abdb0c4ddfa5bceb66a4bed3722ffe91b702acf
Eric Masiello <eric.j.masiello@gmail.com>
2018-07-08 18:53:38 -0400

    Apply common max width to PageContainer

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { pxToRem } from '../styles/utils';
+import { MAX_CONTENT_WIDTH } from '../styles/vars';
 import { TagPropType } from '../propTypes';
 
 export const PageContainer = ({ tag: Tag, ...rest }) => <Tag {...rest} />;
@@ -17,6 +18,6 @@ PageContainer.displayName = 'PageContainer';
 
 export default styled(PageContainer)`
   margin: 0 auto;
-  max-width: ${pxToRem(960)};
+  max-width: ${pxToRem(MAX_CONTENT_WIDTH)};
   padding: 0px 1.0875rem 1.45rem;
 `;
