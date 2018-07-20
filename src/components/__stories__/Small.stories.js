01d9cd2ac431816610c2101c90245c5543ef3390
Eric Masiello <eric.j.masiello@gmail.com>
2018-07-20 08:15:53 -0400

    Remove storybook and lint staged

--- src/components/__stories__/Small.stories.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import Small from '../Small';
-
-const stories = storiesOf('Small', module);
-
-stories.add('default', () => (<Small>Hello world</Small>));
--- src/components/__stories__/Type3.stories.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import Type3 from '../Type3';
-
-const stories = storiesOf('Type3', module);
-
-stories.add('default', () => (<Type3>Hello world</Type3>));
