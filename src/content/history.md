30800b1000f50caa06de8737215413b3538b2b62
Eric Masiello <eric.j.masiello@gmail.com>
2018-07-27 19:59:44 -0600

    Load about content from markdown

---
+++ src/content/history.md
@@ -0,0 +1,24 @@
+Priority Construction Corp. was established in 1996 by Pedro Ponce, a humble man
+from humble beginnings. Pedro Ponce envisioned a family owned company that focused
+on building positive bonds and maintaining a good reputation within the construction
+community in Baltimore. What started off as a small, family owned construction
+company working on 1-2 projects at a time, has transformed into one of the most
+successful minority based subcontractors in the Baltimore metropolitan area.
+
+For the first couple of years, project bids where being made in the basement of his
+family home in Silver Spring, MD, with a total work force of 10 people. As the years
+passed by and more projects where being successfully completed, Priority
+Construction Corp. started to build a reputation of not only finishing the projects
+on time but with a professional craft that speaks for itself.
+
+Baltimore Inner Harbor, Towson University, Oriole Park at Camden Yards, Jones Falls
+Trail, University of Maryland College Park, Metro Pointe in Wheaton, are just a few
+projects that have been completed by the company and has the parts in Maryland with
+a better view.
+
+The driving force behind Priority Construction Corp. success has been and will
+always be the individuals working for the company. Whether it's the Finisher
+or Project Estimator, everyone's unique set of skills makes this company
+always strive for success and their ability to solve any challenge they receive
+makes Priority Construction Corp. an industry standard when it comes to the concrete
+and brick paving business.
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-import List from '../components/List';
 import Small from '../components/Small';
 import PageContainer from '../components/PageContainer';
 import ContentBlock from '../components/ContentBlock';
@@ -12,6 +11,7 @@ import { pxToRem } from '../styles/utils';
 import { COLORS, PAGE_SPACING, MAX_CONTENT_WIDTH } from '../styles/vars';
 import { withLayoutContext } from '../layoutContext';
 import { hasScrolledPastBottomOfElement } from '../utils/ui';
+import * as CustomPropTypes from '../propTypes';
 
 const StickyNavContainer = styled.div`
   display: none;
@@ -142,7 +142,7 @@ class About extends React.Component {
   };
 
   render() {
-    const { className } = this.props;
+    const { className, data } = this.props;
 
     return (
       <PageContainer tag="section" className={className}>
@@ -187,58 +187,24 @@ class About extends React.Component {
           </div>
 
           <div>
-            <ContentBlock tag="section" id="history" tabIndex={-1}>
-              <p>
-                Priority Construction Corp. was established in 1996 by Pedro Ponce, a humble man
-                from humble beginnings. Pedro Ponce envisioned a family owned company that focused
-                on building positive bonds and maintaining a good reputation within the construction
-                community in Baltimore. What started off as a small, family owned construction
-                company working on 1-2 projects at a time, has transformed into one of the most
-                successful minority based subcontractors in the Baltimore metropolitan area.
-              </p>
-              <p>
-                For the first couple of years, project bids where being made in the basement of his
-                family home in Silver Spring, MD, with a total work force of 10 people. As the years
-                passed by and more projects where being successfully completed, Priority
-                Construction Corp. started to build a reputation of not only finishing the projects
-                on time but with a professional craft that speaks for itself.
-              </p>
-              <p>
-                Baltimore Inner Harbor, Towson University, Oriole Park at Camden Yards, Jones Falls
-                Trail, University of Maryland College Park, Metro Pointe in Wheaton, are just a few
-                projects that have been completed by the company and has the parts in Maryland with
-                a better view.
-              </p>
-              <p>
-                The driving force behind Priority Construction Corp. success has been and will
-                always be the individuals working for the company. Whether it&rsquo;s the Finisher
-                or Project Estimator, everyone&rsquo;s unique set of skills makes this company
-                always strive for success and their ability to solve any challenge they receive
-                makes Priority Construction Corp. an industry standard when it comes to the concrete
-                and brick paving business.
-              </p>
-            </ContentBlock>
-            <ContentBlock tag="section" id="mission" tabIndex={-1}>
-              <p>
-                At Priority Construction, we strive to exceed our client’s and competitor&rsquo;s
-                expectation by maintaining a high level of professionalism, integrity, workmanship,
-                work ethic, and honesty. We value the importance of our relationships and will
-                continue to remain fair and true in our dealings with all employees and clients.
-              </p>
-              <p>
-                Our clients count on our dependability and drive. We take price in that. We take
-                pride in our accomplishments and build on them every day.
-              </p>
-            </ContentBlock>
-            <ContentBlock tag="section" id="certifications" tabIndex={-1}>
-              <List>
-                <List.Item>MBE - STATE # 02-490</List.Item>
-                <List.Item>MBE - BALTIMORE CITY # 01-003989</List.Item>
-                <List.Item>MDOT # 02-490 - MBE Eligible Letter</List.Item>
-                <List.Item>MDOT # 02-490 - Expansion Of Services Letter</List.Item>
-                <List.Item>Baltimore County Certificate of Prequalification</List.Item>
-              </List>
-            </ContentBlock>
+            <ContentBlock
+              tag="section"
+              id="history"
+              tabIndex={-1}
+              dangerouslySetInnerHTML={{ __html: data.history.html }}
+            />
+            <ContentBlock
+              tag="section"
+              id="mission"
+              tabIndex={-1}
+              dangerouslySetInnerHTML={{ __html: data.mission.html }}
+            />
+            <ContentBlock
+              tag="section"
+              id="certifications"
+              tabIndex={-1}
+              dangerouslySetInnerHTML={{ __html: data.certifications.html }}
+            />
           </div>
         </AboutContent>
       </PageContainer>
@@ -250,6 +216,11 @@ About.displayName = 'About';
 
 About.propTypes = {
   className: PropTypes.string,
+  data: PropTypes.shape({
+    mission: CustomPropTypes.Markdown,
+    history: CustomPropTypes.Markdown,
+    certifications: CustomPropTypes.Markdown,
+  }),
   navRef: PropTypes.shape({
     current: PropTypes.shape({}),
   }),
@@ -257,4 +228,20 @@ About.propTypes = {
 
 export default styled(withLayoutContext(About))`
   padding-top: 2rem;
+`;
+
+export const query = graphql`
+  query AboutPage {
+    mission: markdownRemark(id: { regex: "/content/mission/" }) {
+      html
+    }
+
+    history: markdownRemark(id: { regex: "/content/history/" }) {
+      html
+    }
+
+    certifications: markdownRemark(id: { regex: "/content/certifications/" }) {
+      html
+    }
+  }
 `;
