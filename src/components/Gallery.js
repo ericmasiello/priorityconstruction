b260d51ae019baf7eb0ad33f2076231ebdce75c5
Eric Masiello <eric.j.masiello@gmail.com>
2018-08-12 16:56:36 -0400

    Update styles to use pxToRem

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,9 @@
 import List from './List';
+import { pxToRem } from '../styles/utils';
 
 const Gallery = List.extend`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(${pxToRem(300)}, 1fr));
   grid-gap: 0.5rem;
 `;
 
