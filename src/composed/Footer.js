fdfe851f3229caa14be45b8655759b08091d50f0
Eric Masiello <eric.j.masiello@gmail.com>
2018-10-07 18:43:29 -0400

    Update design of footer

--- src/composed/Footer.js
+++ src/composed/Footer.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import tinyColor from 'tinycolor2';
 import Link from 'gatsby-link';
-import Footer from '../components/Footer';
 import List from '../components/List';
 import ListItem from '../components/ListItem';
+import Container from '../components/Container';
 import { pxToRem } from '../styles/utils';
-import { COLORS, TYPE_SIZE } from '../styles/vars';
+import { COLORS, TYPE_SIZE, GUTTER_SIZE } from '../styles/vars';
+import * as CustomPropTypes from '../propTypes';
 
-const Container = styled.div`
-  @media (min-width: ${pxToRem(650)}) {
-    display: flex;
-    justify-content: space-between;
+const BackgroundContainer = styled(Container)`
+  padding: ${pxToRem(40)} ${pxToRem(GUTTER_SIZE)};
+  background-color: ${tinyColor(COLORS.brand[0])
+    .setAlpha(0.9)
+    .toRgbString()};
+
+  &,
+  a {
+    color: #fff;
   }
 `;
 
@@ -26,14 +33,13 @@ const Address = styled.address`
 
   ${ListItem} {
     margin-bottom: 0;
-    border-left: 1px solid ${COLORS.base};
-    padding-left: ${pxToRem(8)};
     padding-right: ${pxToRem(8)};
-  }
 
-  ${ListItem}:first-child {
-    padding-left: 0;
-    border-left: none;
+    &:not(:last-child)::after {
+      content: '·';
+      display: inline-block;
+      padding-left: 0.5rem;
+    }
   }
 
   ${ListItem}:last-child {
@@ -42,51 +48,50 @@ const Address = styled.address`
 `;
 
 export const ComposedFooter = props => {
-  const { streetAddress, city, state, zip, phone, ...rest } = props;
+  const { tag: Tag, streetAddress, city, state, zip, phone, ...rest } = props;
   return (
-    <Footer {...rest}>
-      <Container>
-        <div>
-          <Address>
-            <a href="https://www.google.com/maps/place/1315+W+Hamburg+St,+Baltimore,+MD+21230/">
-              <List>
-                <ListItem>
-                  {streetAddress} 
-                  {' '}
-                  {city}
-                  {', '}
-                  {state} 
-                  {' '}
-                  {zip}
-                </ListItem>
-              </List>
-            </a>
+    <Tag {...rest}>
+      <BackgroundContainer>
+        <Address>
+          <a href="https://www.google.com/maps/place/1315+W+Hamburg+St,+Baltimore,+MD+21230/">
             <List>
               <ListItem>
-                <a href={`tel:${phone}`}>
-                  {'phone: '}
-                  {phone}
-                </a>
+                {streetAddress} 
+                {' '}
+                {city}
+                {', '}
+                {state} 
+                {' '}
+                {zip}
               </ListItem>
             </List>
-            <List>
-              <ListItem>
-                <Link to="/contact">Contact Us</Link>
-              </ListItem>
-              <ListItem>
-                <Link to="/careers">Career Opportunities</Link>
-              </ListItem>
-            </List>
-          </Address>
-        </div>
-      </Container>
-    </Footer>
+          </a>
+          <List>
+            <ListItem>
+              <a href={`tel:${phone}`}>
+                {'phone: '}
+                {phone}
+              </a>
+            </ListItem>
+          </List>
+          <List>
+            <ListItem>
+              <Link to="/contact">Contact Us</Link>
+            </ListItem>
+            <ListItem>
+              <Link to="/careers">Career Opportunities</Link>
+            </ListItem>
+          </List>
+        </Address>
+      </BackgroundContainer>
+    </Tag>
   );
 };
 
 ComposedFooter.displayName = 'ComposedFooter';
 
 ComposedFooter.propTypes = {
+  tag: CustomPropTypes.Tag,
   streetAddress: PropTypes.string.isRequired,
   city: PropTypes.string.isRequired,
   state: PropTypes.string.isRequired,
@@ -94,7 +99,21 @@ ComposedFooter.propTypes = {
   phone: PropTypes.string.isRequired,
 };
 
+ComposedFooter.defaultProps = {
+  tag: 'footer',
+};
+
 export default styled(ComposedFooter)`
   font-size: ${pxToRem(TYPE_SIZE.small[0])};
   line-height: ${TYPE_SIZE.small[1]};
+
+  ${Address} {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  ${List} {
+    display: inline-flex;
+  }
 `;
