06834b39063ae1c9f57827064b38e6e620f80138
Eric Masiello <eric.j.masiello@gmail.com>
2018-10-24 18:15:21 -0400

    Remove unused methods

--- src/utils/form.js
+++ src/utils/form.js
@@ -3,39 +3,6 @@ export const encode = data =>
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
     .join('&');
 
-export const fieldsToKeyValues = fields =>
-  Object.keys(fields).reduce((acc, key) => {
-    acc[key] = fields[key].value;
-    return acc;
-  }, {});
-
-export const fieldsToFieldState = (fields, onChange) => {
-  if (Array.isArray(fields)) {
-    return fields.reduce((acc, field) => {
-      acc[field] = {
-        name: field,
-        onChange,
-        value: '',
-      };
-      return acc;
-    }, {});
-  }
-  if (typeof fields === 'object') {
-    return Object.keys(fields).reduce((acc, key) => {
-      acc[key] = Object.assign(
-        {
-          name: key,
-          onChange,
-          value: '',
-        },
-        fields[key],
-      );
-      return acc;
-    }, {});
-  }
-  return {};
-};
-
 export const validateFields = (validationFields, values) => {
   const defaultValidationResult = {
     hasErrors: false,
