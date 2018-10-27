e3ac3543fdb754d8f905675ac7482b5980a8da77
Eric Masiello <eric.j.masiello@gmail.com>
2018-10-27 14:00:40 -0400

    Change alignment of success message so it appears above the fold on long forms

--- src/components/FormSuccessMessage.js
+++ src/components/FormSuccessMessage.js
@@ -6,18 +6,24 @@ const FormSuccessMessage = styled.div`
   color: #fff;
   display: flex;
   justify-content: center;
-  align-items: center;
   text-align: center;
   position: absolute;
   top: 0;
   left: 0;
   right: 0;
-  transition: height 0.5s;
+  transition: height 0.5s, padding 0.8s;
   overflow: hidden;
   padding-left: 3vw;
   padding-right: 3vw;
   z-index: 3;
-  ${({ show }) => (show ? `height: 100%` : `height: 0`)};
+  ${({ show }) =>
+    show
+      ? `
+          height: 100%;
+          padding-top: 6vh;
+          padding-bottom: 6vh;
+      `
+      : `height: 0`};
 `;
 
 export default FormSuccessMessage;
