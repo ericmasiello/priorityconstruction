b35f8fbdd02450d6bb4874f96891c9e3b936edfe
Eric Masiello <eric.j.masiello@gmail.com>
2018-10-28 16:36:10 -0400

    Add support for decorated lists

--- src/components/List.js
+++ src/components/List.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 import * as CustomPropTypes from '../propTypes';
 
-export const List = ({ tag: Tag, ...rest }) => <Tag {...rest} />;
+export const List = ({ tag: Tag, decorated, ...rest }) => <Tag {...rest} />;
 
 List.propTypes = {
   tag: CustomPropTypes.Tag,
+  decorated: PropTypes.bool,
 };
 
 List.defaultProps = {
   tag: 'ul',
+  decorated: false,
 };
 
 List.displayName = 'List';
 
 const StyleList = styled(List)`
-  list-style-type: none;
   margin: 0;
-  padding: 0;
+  padding-left: 1.5rem;
+
+  ${({ decorated }) =>
+    !decorated &&
+    `
+    list-style-type: none;
+    padding-left: 0;
+  `};
 `;
+
+StyleList.propTypes = List.propTypes;
+StyleList.defaultProps = List.defaultProps;
 
 export default StyleList;
