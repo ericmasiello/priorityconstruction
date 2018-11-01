278df4437a29ec07f679aa8f2449ab0e60eb4a30
Eric Masiello <eric.j.masiello@gmail.com>
2018-11-01 19:53:18 -0400

    Add design for intro and awards

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import tinyColor from 'tinycolor2';
 import PageContainer from '../components/PageContainer';
 import MarkdownBlock from '../components/MarkdownBlock';
+import TrophyIcon from '../components/TrophyIcon';
+import Type4 from '../components/Type4';
 import * as CustomPropTypes from '../propTypes';
+import { pxToRem } from '../styles/utils';
+import { TYPE_SIZE, COLORS } from '../styles/vars';
+
+const Intro = styled(MarkdownBlock)`
+  font-size: ${pxToRem(TYPE_SIZE.t4[0])};
+  line-height: ${TYPE_SIZE.t4[1]};
+  text-align: center;
+  margin-bottom: 3rem;
+
+  ul {
+    columns: 2;
+  }
+`;
+
+const Awards = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  align-items: center;
+  margin-bottom: 3rem;
+
+  ${MarkdownBlock} {
+    padding: 2rem;
+    border: 1px solid ${COLORS.gray};
+    background-color: ${tinyColor(COLORS.gray)
+      .setAlpha(0.2)
+      .toRgbString()};
+  }
+`;
+
+const AwardHeader = styled.header`
+  justify-self: center;
+  text-align: center;
+`;
+
+const AwardIcon = styled(TrophyIcon)`
+  fill: #333e4f;
+  width: ${pxToRem(104)};
+`;
 
 class About extends React.PureComponent {
   static displayName = 'About';
@@ -24,14 +65,20 @@ class About extends React.PureComponent {
 
     return (
       <PageContainer tag="section" className={className}>
-        <MarkdownBlock tag="section" dangerouslySetInnerHTML={{ __html: data.intro.html }} />
-        <MarkdownBlock tag="section" dangerouslySetInnerHTML={{ __html: data.history.html }} />
+        <Intro tag="section" dangerouslySetInnerHTML={{ __html: data.intro.html }} />
+        <Awards>
+          <AwardHeader>
+            <Type4 tag="h1">Awards</Type4>
+            <AwardIcon />
+          </AwardHeader>
+          <MarkdownBlock dangerouslySetInnerHTML={{ __html: data.awards.html }} />
+        </Awards>
         <MarkdownBlock tag="section" dangerouslySetInnerHTML={{ __html: data.mission.html }} />
+        <MarkdownBlock tag="section" dangerouslySetInnerHTML={{ __html: data.history.html }} />
         <MarkdownBlock
           tag="section"
           dangerouslySetInnerHTML={{ __html: data.certifications.html }}
         />
-        <MarkdownBlock tag="section" dangerouslySetInnerHTML={{ __html: data.awards.html }} />
       </PageContainer>
     );
   }
