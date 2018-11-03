5fb0ff1cc979ca33f0e444d660cbcfe8be3d2529
Eric Masiello <eric.j.masiello@gmail.com>
2018-11-02 20:23:46 -0400

    Add history

--- src/styles/utils.js
+++ src/styles/utils.js
@@ -1,2 +1,5 @@
-/* eslint-disable import/prefer-default-export */
+import { GUTTER_SIZE, GRID_SIZE } from './vars';
+
 export const pxToRem = (value, base = 16) => `${value / base}rem`;
+
+export const grids = (count = 1) => GRID_SIZE * count + GUTTER_SIZE * (count - 1);
