95730f6e5d538e70f880e5aef4381ea0b754757a
Eric Masiello <eric.j.masiello@gmail.com>
2018-11-04 16:31:22 -0500

    Add Concrete Flatwork design

--- src/pages/services.js
+++ src/pages/services.js
@@ -3,21 +3,32 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import GatsbyImage from '../components/GatsbyImage';
 import PageContainer from '../components/PageContainer';
+import Container from '../components/Container';
 import MarkdownBlock from '../components/MarkdownBlock';
 import * as CustomPropTypes from '../propTypes';
 import Type3 from '../components/Type3';
 import mergeServiceWithImages from '../utils/services';
-import { TYPE_SIZE } from '../styles/vars';
+import { TYPE_SIZE, COLORS } from '../styles/vars';
 import { pxToRem, grids } from '../styles/utils';
 import { type } from '../styles/mixins';
 
-const Intro = styled.section`
+const Content = styled.div``;
+
+const Intro = styled(Container)`
   margin-bottom: 3rem;
 
   @media (min-width: ${pxToRem(750)}) {
     display: grid;
     grid-template-columns: minmax(50%, 1fr) minmax(${pxToRem(200)}, ${pxToRem(grids(5))});
     column-gap: ${pxToRem(grids(1))};
+  }
+
+  ${Content} {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
   }
 
   ${MarkdownBlock} {
@@ -29,12 +40,61 @@ const Intro = styled.section`
   }
 `;
 
-const IntroContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+const ConcreteFlatwork = styled(Container)`
+  margin-bottom: 3rem;
+  background-color: ${COLORS.gray[1]};
+  padding: 1rem;
+
+  @media (min-width: ${pxToRem(750)}) {
+    display: grid;
+    grid-gap: 1rem;
+    grid-template-columns: repeat(8, 1fr) ${(4 / 12) * 100}%;
+  }
+
+  ${Content} {
+    grid-column: 1 / 9;
+    grid-row: 1 / 2;
+    background-color: ${COLORS.brand[0]};
+    color: #fff;
+    padding: 1rem;
+  }
+
+  ${GatsbyImage} {
+    height: 100%;
+  }
+
+  ${MarkdownBlock} {
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+    }
+
+    li {
+      &:not(:last-child)::after {
+        content: '·';
+        font-weight: bold;
+        margin-left: 0.5rem;
+        margin-right: 0.5rem;
+      }
+    }
+  }
+
+  .gatsby-image-outer-wrapper {
+    &:nth-child(2) {
+      grid-column: 9 / -1;
+      grid-row: 1 / 3;
+    }
+
+    &:nth-child(3) {
+      grid-column: 1 / 6;
+      grid-row: 2 / 3;
+    }
+
+    &:nth-child(4) {
+      grid-column: 6 / 9;
+      grid-row: 2 / 3;
+    }
+  }
 `;
 
 class Services extends React.PureComponent {
@@ -59,20 +119,29 @@ class Services extends React.PureComponent {
     const structuralConcrete = mergeServiceWithImages(data.structuralConcrete, data.images.edges);
 
     return (
-      <PageContainer tag="section" className={className}>
-        <Intro>
-          <IntroContent>
+      <PageContainer plus className={className}>
+        <Intro tag="section">
+          <Content>
             <Type3 tag="h1" uppercase bold>
               {intro.title}
             </Type3>
             <MarkdownBlock dangerouslySetInnerHTML={{ __html: intro.content }} />
-          </IntroContent>
+          </Content>
           <GatsbyImage {...intro.images[0]} />
         </Intro>
-        <section>
-          <Type3 tag="h1">{concreteFlatwork.title}</Type3>
-          <MarkdownBlock dangerouslySetInnerHTML={{ __html: concreteFlatwork.content }} />
-        </section>
+
+        <ConcreteFlatwork plus tag="section">
+          <Content>
+            <Type3 tag="h1" uppercase bold>
+              {concreteFlatwork.title}
+            </Type3>
+            <MarkdownBlock dangerouslySetInnerHTML={{ __html: concreteFlatwork.content }} />
+          </Content>
+          {concreteFlatwork.images.map(image => (
+            <GatsbyImage {...image} key={image.id} />
+          ))}
+        </ConcreteFlatwork>
+
         <section>
           <Type3 tag="h1">{hardscapes.title}</Type3>
           <MarkdownBlock dangerouslySetInnerHTML={{ __html: hardscapes.content }} />
