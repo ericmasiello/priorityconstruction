51ef71c25dbcd856a9b1442eea349d8e9e84cc8b
Eric Masiello <eric.j.masiello@gmail.com>
2018-11-04 15:20:50 -0500

    Apply design to intro

--- src/content/services/intro.md
+++ src/content/services/intro.md
@@ -3,6 +3,6 @@ title: 'Services'
 images: [{ image: 'services/priority-worker.jpg', alt: 'Priority construction worker' }]
 ---
 
-Introductory paragraph about your expertise in concrete should go here.
+Introductory paragraph about your expertise in concrete should go&nbsp;here.
 
 Mention the planning process, can provide design services, project management/overseeing from start to finish
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import GatsbyImage from '../components/GatsbyImage';
 import PageContainer from '../components/PageContainer';
 import MarkdownBlock from '../components/MarkdownBlock';
 import * as CustomPropTypes from '../propTypes';
 import Type3 from '../components/Type3';
 import mergeServiceWithImages from '../utils/services';
+import { TYPE_SIZE } from '../styles/vars';
+import { pxToRem, grids } from '../styles/utils';
+import { type } from '../styles/mixins';
+
+const Intro = styled.section`
+  margin-bottom: 3rem;
+
+  @media (min-width: ${pxToRem(750)}) {
+    display: grid;
+    grid-template-columns: minmax(50%, 1fr) minmax(${pxToRem(200)}, ${pxToRem(grids(5))});
+    column-gap: ${pxToRem(grids(1))};
+  }
+
+  ${MarkdownBlock} {
+    ${type(TYPE_SIZE.t5)};
+  }
+
+  .gatsby-image-outer-wrapper {
+    grid-column: 2 / -1;
+  }
+`;
+
+const IntroContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
 
 class Services extends React.PureComponent {
   static displayName = 'Services';
@@ -23,7 +53,6 @@ class Services extends React.PureComponent {
 
   render() {
     const { className, data } = this.props;
-
     const intro = mergeServiceWithImages(data.intro, data.images.edges);
     const concreteFlatwork = mergeServiceWithImages(data.concreteFlatwork, data.images.edges);
     const hardscapes = mergeServiceWithImages(data.hardscapes, data.images.edges);
@@ -31,10 +60,15 @@ class Services extends React.PureComponent {
 
     return (
       <PageContainer tag="section" className={className}>
-        <section>
-          <Type3 tag="h1">{intro.title}</Type3>
-          <MarkdownBlock dangerouslySetInnerHTML={{ __html: intro.content }} />
-        </section>
+        <Intro>
+          <IntroContent>
+            <Type3 tag="h1" uppercase bold>
+              {intro.title}
+            </Type3>
+            <MarkdownBlock dangerouslySetInnerHTML={{ __html: intro.content }} />
+          </IntroContent>
+          <GatsbyImage {...intro.images[0]} />
+        </Intro>
         <section>
           <Type3 tag="h1">{concreteFlatwork.title}</Type3>
           <MarkdownBlock dangerouslySetInnerHTML={{ __html: concreteFlatwork.content }} />
