65c648358d30fb5556dab0114dbc93b58ac61285
Eric Masiello <eric.j.masiello@gmail.com>
2018-11-20 14:51:33 -0500

    Enforce sorting of the photos to match the order in the markdown file

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,22 +1,27 @@
 const mergeServiceWithImages = (service = {}, imageEdges = []) => {
   const { frontmatter: { title, images: imageMeta } = {}, html: content = '' } = service;
 
-  const images = imageEdges.reduce((acc, { node }) => {
-    const match = imageMeta.find(meta => node.id.includes(meta.image));
-
-    if (match) {
-      acc.push({
+  const imageMap = imageEdges.reduce((acc, { node }) => {
+    const index = imageMeta.findIndex(meta => node.id.includes(meta.image));
+    if (index > -1) {
+      acc[index] = {
         ...node,
-        alt: match.alt,
-      });
+        alt: imageMeta[index].alt,
+      };
     }
     return acc;
-  }, []);
+  }, {});
 
   return {
     title,
     content,
-    images,
+    // sorts the imges by key
+    images: Object.keys(imageMap)
+      .sort()
+      .reduce((acc, key) => {
+        acc.push(imageMap[key]);
+        return acc;
+      }, []),
   };
 };
 
